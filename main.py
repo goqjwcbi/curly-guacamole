9c97dd6336d818a064ab59506b58f5584e05105c
5avahn <no>
2020-05-31 03:46:02 +0000

    added login, register, stuff and check_user_exists and some html files

---
+++ main.py
@@ -0,0 +1,64 @@
+from flask import Flask, request
+import sqlite3
+import hashlib
+
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='static')
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
+@app.route("/")
+def index():
+    if 'user' in session:
+        return "welcome %s" % session['user']
+    else:
+        return render_template("index.html")
+    
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    if method == "POST":
+        username = request.args.get("username")
+        password = hashlib.sha256(request.args.get("password")).hexdigest()
+
+        c = sqlite.conn("guacamole.db").cursor()
+        query = "SELECT username FROM users WHERE name = ? AND password = ?"
+        c.execute(query, username, password)
+        data = c.fetchone()
+        if data == None:
+            printf("failed to log in as %s" % username)
+        else:
+            session['user'] = username
+        redirect(url_for(""))
+    else:
+        return "unsupportted method"
+        
+@app.route("/register", methods = ["GET", "POST"])
+def register():
+    if method == "POST":
+        username = request.args.get("username")
+        password = request.args.get("password")
+        if not check_user_exists(username):
+            query = "INSERT INTO users VALUES (?, ?)"
+            c = sqlite.conn("guacamole.db").cursor()
+            c.execute(query, username, password)
+            redirect(url_for(""))
+        else:
+            return error("user already exists")
+    else:
+        return "unsupported method"
+
+
+def error(e):
+    return render_template("error.html", error = e)
+            
+def check_user_exists(username):
+    c = sqlite.conn().cursor()
+    query = "SELECT username FROM users WHERE name = ?"
+    c.execute(query, username)
+    data = c.fetchone()
+    if data == None:
+        return False
+    else:
+        return True
